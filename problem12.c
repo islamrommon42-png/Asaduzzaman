62b287a72fc6a2b0a449a173d39582edae270b19
Muhammad Asaduzzaman <islamrommon42@gmail.com>
2026-05-27 00:01:03 +0600

    Array with char

---
+++ problem12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+char *a[6];
+a[0]="Rajshahi";
+a[1]="Dhaka";
+a[2]="Chattogram";
+a[3]="Khulna";
+a[4]="Barisal";
+a[5]='R';
+
+
+printf("%s\n",a[0]);
+printf("%s\n",a[1]);
+printf("%s\n",a[2]);
+printf("%s\n",a[3]);
+printf("%s\n",a[4]);
+printf("%c\n",a[5]);
+}
